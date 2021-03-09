8fa7a4a5f679e3125b01a88e8451534a0b867bc0
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2021-03-09 15:33:20 +0100

    seed db with data.json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'json'
+
+puts 'seeding db from data.json'
+
+data = JSON.parse(File.open('data.json').read)
+
+puts 'creating users'
+users = data['users']
+
+users.map do |user|
+  uid = user['id']
+
+  user = User.find_or_create_by!(uid: uid)
+
+  user.update!(
+    uid: uid,
+    btc_balance: user['btc_balance'],
+    eur_balance: user['eur_balance']
+  )
+end
+
+puts 'users created'
+
+puts 'creating queued_orders'
+
+queued_orders = data['queued_orders']
+
+queued_orders.map do |q|
+
+  QueuedOrder.create!(
+    user_id: q['user_id'],
+    direction: q['direction'],
+    btc_amount: q['btc_amount'],
+    price: q['price']
+  )
+end
+
+puts 'queued_orders created'
+
